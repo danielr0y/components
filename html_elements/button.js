2a40a986ff8b161437e55892f219c00ab0c04b04
Daniel Roy <daniel@thevariousartists.com.au>
2022-10-27 16:20:20 +1000

    adds html button element component

---
+++ html_elements/button.js
@@ -0,0 +1,46 @@
+const button = function( button ) 
+{
+
+    /* 
+     * methods
+     */
+    function enable()
+    {
+        button.disabled = false;
+    }
+    
+
+
+    function disable()
+    {
+        button.disabled = true;
+    }
+    
+
+
+    function click()
+    {
+        button.dispatchEvent( new Event('click') );
+    }
+    
+
+
+    /* 
+     * run it
+     */
+    if ( ! button instanceof HTMLButtonElement)
+    {
+        throw new TypeError('element is not a button');
+    }
+    
+
+
+    /* 
+     * interface
+     */
+    return {
+        enable,
+        disable,
+        click
+    };
+};
